f95d4bee841d018caf910dd32e5244a24e54c61a
mayroga <132472344+mayroga@users.noreply.github.com>
2025-12-04 18:45:30 -0500

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,5 @@
+LEGAL_DISCLAIMER_CORE = (
+    "Aviso legal: Toda mercancía que contenga productos peligrosos o restringidos "
+    "debe ser declarada correctamente. El sistema no se hace responsable de "
+    "incumplimientos de la normativa internacional."
+)
