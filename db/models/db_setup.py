adc4d3cf4fbfb712c462824936359300fc7c4a42
mayroga <132472344+mayroga@users.noreply.github.com>
2025-12-04 19:18:47 -0500

    Delete db/models directory

--- db/models/db_setup.py
+++
@@ -1,9 +0,0 @@
-# SMARTCARGO-AIPA/db/models/db_setup.py
-
-from sqlalchemy.orm import declarative_base
-
-# Definición de la Base
-Base = declarative_base()
-
-# La configuración de la conexión (create_engine, sessionmaker) debe estar en un 
-# archivo de inicialización de la aplicación o en la instancia de Flask-SQLAlchemy.
